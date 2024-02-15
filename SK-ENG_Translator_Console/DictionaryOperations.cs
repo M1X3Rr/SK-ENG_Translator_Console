db6b39fef0f79269d7ead31cb47e606782a4efb5
jamix <jamix@DUNDERBOT>
2024-02-15 11:17:42 +0100

    Add project files.

---
+++ SK-ENG_Translator_Console/DictionaryOperations.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryOperations
+{
+    internal class DictionaryOperations
+    {
+        public static string Translate(Dictionary<string, string> dict, string key)
+        {
+            return dict.TryGetValue(key, out string value) ? value : null;
+        }    
+
+        public static void AddWordToDict(Dictionary<string, string> dictionary, string key)
+        {
+            Console.Clear();
+            while (true)
+            {
+                if(key == null)
+                {
+                    Console.WriteLine("Write the Slovak word to add to the dictionary: \n(write \"cancel\" to cancel the operation)\n");
+                    key = Console.ReadLine();
+                }
+                
+                if (key.ToLower() == "cancel")
+                {
+                    CancelOp();
+                }
+
+                if (IsString(key))
+                {
+                    Console.WriteLine("Write the corresponding English translation:");
+                    string value = Console.ReadLine();
+
+                    if (IsString(value))
+                    {
+                        if (!dictionary.ContainsKey(key))
+                        {
+                            dictionary.Add(key, value);
+                            Console.WriteLine($"Word \"{key}\" successfully added to the dictionary.");
+                            Console.ReadLine();
+                            return;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"The word \"{key}\" already exists in the dictionary.");
+                            Console.ReadLine();
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input for translation. Please enter a string.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input for Slovak word. Please enter a string.");
+                }
+            }
+        }
+
+        public static void GetDict(Dictionary<string, string> dictionary)
+        {
+            while(true)
+            {
+                Console.Clear();
+                Console.WriteLine("What would you like to get:\na - all the words from dictionary\nn - number of words in dictionary\nc - cancel");
+                char choice = Console.ReadKey().KeyChar;
+
+                switch (choice)
+                {
+                    case 'a':
+                        PrintAll(dictionary);
+                        break;
+                    case 'n':
+                        GetNumberOfWords(dictionary);
+                        break;
+
+                    case 'c':
+                        CancelOp();
+                        return;
+
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Invalid input! Pleas try a valid choice!");
+                        Console.ReadLine();
+                        return;
+                }
+            }
+        }
+
+        private static void PrintAll(Dictionary<string, string> dictionary)
+        {
+            Console.Clear();
+            Console.WriteLine("Dictionary Contents:");
+            foreach (var pair in dictionary)
+            {
+                Console.WriteLine($"\n> Slovak: {pair.Key}, English: {pair.Value}");
+                Console.WriteLine( ">------------------------------------------<");
+            }
+            Console.WriteLine("\n\nPress enter to continue...");
+            Console.ReadLine();
+        }
+
+        private static void GetNumberOfWords(Dictionary<string, string> dictionary)
+        {
+            Console.Clear();
+            int numberOfWords = dictionary.Count;
+            Console.WriteLine($"Number of words in the dictionary: {numberOfWords}");
+            Console.WriteLine("\n\nPress enter to continue...");
+            Console.ReadLine();
+        }
+
+
+        private static bool IsString(object variable)
+        {
+            return variable is string;
+        }
+
+        private static void CancelOp()
+        {
+            Random random = new Random();
+            for (int i = 3; i != 0; i--)
+            {
+                Console.Clear();
+                Console.WriteLine($"Canceling...{i}");
+                Thread.Sleep(random.Next(100, 401));
+            }
+            Console.Clear();
+            return; // Cancel the operation
+        }
+    }
+}
---
+++ SK-ENG_Translator_Console/Program.cs
@@ -0,0 +1,103 @@
+﻿/***********************************************************
+ * Console Application, that has a dictionary of Slovak word translated to english
+ * You can add word's to dictionary, 
+ * App starts by initializing the dictionary, with 5 standard words
+ * (counting is just for visuals and takes random time) 
+ ***********************************************************/
+
+
+using System;
+using System.Collections.Generic;
+using DictionaryOperations;
+
+Dictionary<string, string> engToSk = new Dictionary<string, string>();
+
+Console.Write("Starting the Slovak to English translator...");
+InitializeDictionary(engToSk);
+
+Console.WriteLine("\nStarting words loaded...");
+Thread.Sleep(1000);
+Console.Clear();
+
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine("ENGLISH TO SLOVAK TRANSLATOR");
+    Console.WriteLine("\ng - get data\nt - translate your word\ne - exit");
+    char choice = Console.ReadKey().KeyChar;
+
+    switch (choice)
+    {
+        case 'g':
+            DictionaryOperations.DictionaryOperations.GetDict(engToSk);
+            break;
+        case 't':
+            TranslateWord(engToSk);
+            break;
+        case 'e':
+            Console.Clear();
+            Console.WriteLine("Exiting...");
+            return;
+        default:
+            Console.Clear();
+            Console.WriteLine("Invalid input! Please try a valid choice!");
+            Console.ReadLine();
+            break;
+    }
+}
+
+static void InitializeDictionary(Dictionary<string, string> dictionary)
+{
+    int count = 1;
+    LoadWord(dictionary, "pes", "dog", count++);
+    LoadWord(dictionary, "macka", "cat", count++);
+    LoadWord(dictionary, "kon", "horse", count++);
+    LoadWord(dictionary, "lev", "lion", count++);
+    LoadWord(dictionary, "liska", "fox", count++);
+}
+
+
+static void TranslateWord(Dictionary<string, string> dictionary)
+{
+    Console.Clear();
+    Console.WriteLine("Please input the word to translate:");
+
+    string key = Console.ReadLine();
+    string translation = DictionaryOperations.DictionaryOperations.Translate(dictionary, key);
+
+    if (translation != null)
+    {
+        Console.WriteLine($"English translation for \"{key}\" is \"{translation}\".");
+        Console.ReadLine();
+    }
+    else
+    {
+        Console.Clear();
+        Console.WriteLine($"Word \"{key}\" isn't in the dictionary!\nWould you like to add \"{key}\" to the dictionary? (Y/N)");
+
+        char choice = Console.ReadKey().KeyChar;
+
+        switch (choice)
+        {
+            case 'y':
+                DictionaryOperations.DictionaryOperations.AddWordToDict(dictionary, key);
+                break;
+            case 'n':
+                return;
+            default:
+                Console.Clear();
+                Console.WriteLine("Invalid input! Please try a valid choice!");
+                Console.ReadLine();
+                return;
+        }
+    }
+}
+
+
+static void LoadWord(Dictionary<string, string> dictionary, string key, string value, int count)
+{
+    Random random = new Random();
+    dictionary.Add(key, value);
+    Console.Write($"  {count}");
+    Thread.Sleep(random.Next(300, 601));
+}
